6f4e4d0ad750c58ef9aaa5ef62dfbbf55c4ea41f
ManiGarg <manigarg317@gmail.com>
2020-12-04 16:09:20 +0530

    First commit

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeeklyAssesment1.Models
+{
+    public class Student
+    {
+        public int Sid { get; set; }
+        public string Sname { set; get; }
+        public  DateTime SDob { get; set; }
+        public string Scont { get; set; }
+
+
+
+    }
+}
